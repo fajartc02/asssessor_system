412252b9523a5c3d31fcd98e305c90ec286cd547
fajartc02 <fajartc02@gmail.com>
2020-10-31 11:46:18 +0700

    changes setting to hosting

--- config/constants.php
+++ config/constants.php
@@ -1,10 +1,12 @@
 <?php
 
-define ("DB_SERVER","localhost");
-define ("DB_USER","smartand_user_assessor");
-define ("DB_PWD","4d4pt1v3_assessor");
-define ("DB_NAME","smartand_assessor");
+define("DB_SERVER", "smartandonp3.com");
+define("DB_USER", "smartand_user_assessor");
+define("DB_PWD", "4d4pt1v3_assessor");
+define("DB_NAME", "smartand_assessor");
 
-define ("LOC_UPLOAD","http://smartandonp3.com");
+define("LOC_UPLOAD", "http://smartandonp3.com");
 
 ?>
+
+<!-- 'smartandonp3.com' , 'smartand_aas', 'aas_4d4pt1v3', 'smartand_tmmin3_qmms' -->
