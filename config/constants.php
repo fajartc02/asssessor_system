cbbc9d6f1656acb9f1f77412ca693328cdcccccc
fajartc02 <fajartc02@gmail.com>
2020-11-10 08:53:36 +0700

    changes new connection

--- config/constants.php
+++ config/constants.php
@@ -1,11 +1,19 @@
 <?php
 
-define("DB_SERVER", "smartandonp3.com");
-define("DB_USER", "smartand_user_assessor");
-define("DB_PWD", "4d4pt1v3_assessor");
-define("DB_NAME", "smartand_assessor");
+// define("DB_SERVER", "smartandonp3.com");
+// define("DB_USER", "smartand_user_assessor");
+// define("DB_PWD", "4d4pt1v3_assessor");
+// define("DB_NAME", "smartand_assessor");
 
-define("LOC_UPLOAD", "http://smartandonp3.com");
+// define("LOC_UPLOAD", "http://smartandonp3.com");
+
+define("DB_SERVER", "smartandonplant3.com");
+define("DB_USER", "u5364194_assessor");
+define("DB_PWD", "4d4pt1v3_assessor");
+define("DB_NAME", "u5364194_db_assessor");
+
+define("LOC_UPLOAD", "http://smartandonplant3.com");
+define("LOC_IMAGE", LOC_UPLOAD . "/assessor_system/");
 
 ?>
 
